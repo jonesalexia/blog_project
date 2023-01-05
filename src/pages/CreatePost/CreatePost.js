34fc28eed8fef54ca9a42ea43f3aa76764c19530
jonesalexia <alexiasojones@gmail.com>
2023-01-05 10:57:46 -0300

    fix: inserting data in firebase

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -3,6 +3,7 @@ import styles from './CreatePost.module.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuthValue } from '../../context/AuthContext';
+import { useInsertDocument } from '../../hooks/useInsertDocument';
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
@@ -11,8 +12,31 @@ const CreatePost = () => {
   const [tags, setTags] = useState([]);
   const [formError, setFormError] = useState("");
 
+  const {user} = useAuthValue();
+
+  const {insertDocument, response} = useInsertDocument("posts"); 
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError("")
+
+    // validate image URL 
+
+    // create tag array
+
+    // check all values
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags,
+      uid: user.uid,
+      createdBy: user.displayName, 
+    })
+
+    // redirect to homepage
+
   }
  
   return (
@@ -41,10 +65,10 @@ const CreatePost = () => {
             <input type="text" name="tags" placeholder="Insira as tags separadas por vírgula" onChange={(e) => setTags(e.target.value)} value={tags} />
           </label>
           
-          <button className="btn">Criar post</button>
-          {/*!loading && }
-          {loading && <button className="btn" disabled>Aguarde...</button>}
-          {error && <p className="error">{error}</p>*/}
+          
+          {!response.loading && <button className="btn">Criar post</button> }
+          {response.loading && <button className="btn" disabled>Aguarde...</button>}
+          {response.error && <p className="error">{response.error}</p>}
 
         </form>
     </div>
